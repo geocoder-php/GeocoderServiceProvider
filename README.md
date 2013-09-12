dd751dfd6229d306723c77cded800c2c61092f4f
Kévin Gomez <geek63@gmail.com>
2013-09-12 12:49:58 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,54 @@
+GeocoderServiceProvider
+====================
+
+A [Geocoder](http://geocoder-php.org/Geocoder/) service provider for [Silex](http://silex.sensiolabs.org/).
+
+
+## Installation
+
+Install the GeocoderServiceProvider adding `kphoen/geocoder-service-provider` to your composer.json or from CLI:
+
+```
+$ php composer.phar require 'kphoen/geocoder-service-provider:~1.0'
+```
+
+
+## Usage
+
+Initialize it using `register`.
+```php
+<?php
+
+use KPhoen\Provider\GeocoderServiceProvider;
+
+$app->register(new GeocoderServiceProvider());
+```
+
+From PHP:
+```php
+<?php
+
+$app->get('/hello', function() use ($app) {
+    $geocoder = $app['geocoder'];
+
+    // do your stuff
+});
+```
+
+
+## Configuration
+
+The service provider creates the following services:
+
+  * `geocoder`: the Geocoder instance ;
+  * `geocoder.provider`: the provider used by Geocoder ;
+  * `geocoder.adapter`: the HTTP adapter used to get data from remotes APIs.
+
+By default, the `geocoder.provider` service uses FreeGeoIP and the
+`geocoder.adapter` service uses the cURL adapter. Override these services to use
+the adapter/provider you want.
+
+
+## Licence
+
+This provider is released under the MIT license.
---
+++ src/KPhoen/Provider/GeocoderServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace KPhoen\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * A Geocoder service provider for Silex.
+ *
+ * @author Kévin Gomez <contact@kevingomez.fr>
+ */
+class GeocoderServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Application $app)
+    {
+        $this->injectServices($app);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(Application $app)
+    {
+    }
+
+    /**
+     * Injects Geocoder related services in the application.
+     *
+     * @param Application $app
+     */
+    protected function injectServices(Application $app)
+    {
+        $app['geocoder'] = $app->share(function($app) {
+            $geocoder = new \Geocoder\Geocoder();
+            $geocoder->registerProvider($app['geocoder.provider']);
+
+            return $geocoder;
+        });
+
+        $app['geocoder.provider'] = $app->share(function($app) {
+            return new \Geocoder\Provider\FreeGeoIpProvider($app['geocoder.adapter']);
+        });
+
+        $app['geocoder.adapter'] = $app->share(function($app) {
+            return new \Geocoder\HttpAdapter\CurlHttpAdapter();
+        });
+    }
+}
